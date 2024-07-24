7c3448c2e4f98659981b07204e6d83f2f5eb20d6
Dev Keshwani <dev.keshwani345@gmail.com>
2024-07-24 17:23:19 +0530

    stage:4 check:0

--- app/server.go
+++ app/server.go
@@ -16,17 +16,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn,err := l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	req:=make([]byte,1024)
-	conn.Read(req)
-	if !strings.HasPrefix(string(req),"GET / HTTP/1.1"){
+	buffer := make([]byte, 1024)
+	conn.Read(buffer)
+	splitHeader := strings.Split(string(buffer), "\r\n")
+	splitRequestLine := strings.Split(splitHeader[0], " ")
+
+	if splitRequestLine[1] == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.Split(splitRequestLine[1], "/")[1] == "echo" {
+		requestBody := strings.Split(splitRequestLine[1], "/")[2]
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(requestBody)) + "\r\n\r\n" + requestBody))
+	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
-		return
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
